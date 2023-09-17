0422d99f4b3299652a8753f210e07babe5e9f36a
GregorJosh <grzegorz.jozw@gmail.com>
2023-09-17 17:56:25 +0200

    init

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -27,7 +27,8 @@ const MovieDetails = () => {
 
   const { poster_path, title, release_date, vote_average, overview, genres } =
     movie;
-  const { card, content, links, link, active, filters, details } = styles;
+  const { section, card, content, links, link, active, filters, details } =
+    styles;
 
   const setClass = ({ isActive }) => (isActive ? active : link);
 
@@ -37,51 +38,56 @@ const MovieDetails = () => {
 
   return (
     <>
-      <Link className={link} to={backLinkHRef}>
+      <Link className={styles['back-link']} to={backLinkHRef}>
         &larr; Go Back
       </Link>
-      <section className={card}>
-        <img src={poster_path} alt={`${title} movie poster`} />
-        <div className={content}>
-          <header>
-            <h1>{`${title} (${release_date})`}</h1>
-            <p>User score: {vote_average}</p>
-          </header>
-          <div>
-            <h3>Overview: </h3>
-            <p>{overview}</p>
-          </div>
-          <div>
-            <h3>Genres: </h3>
-            <p>{genres.map(genre => genre.name + ' ')}</p>
+
+      <section className={section}>
+        <div className={card}>
+          <img src={poster_path} alt={`${title} movie poster`} />
+          <div className={content}>
+            <header>
+              <h1>{`${title} (${release_date})`}</h1>
+              <p>User score: {vote_average}</p>
+            </header>
+            <div>
+              <h3>Overview: </h3>
+              <p>{overview}</p>
+            </div>
+            <div>
+              <h3>Genres: </h3>
+              <p>{genres.map(genre => genre.name + ' ')}</p>
+            </div>
           </div>
         </div>
-      </section>
-      <div className={filters}>
-        <p>Additional information: </p>
-        <ul className={links}>
-          <li>
-            <NavLink
-              className={setClass}
-              to="cast"
-              state={{ from: backLinkHRef }}
-            >
-              Cast
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={setClass}
-              to="reviews"
-              state={{ from: backLinkHRef }}
-            >
-              Reviews
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-      <section className={details}>
-        <Outlet />
+
+        <div className={filters}>
+          <p>Additional information: </p>
+          <ul className={links}>
+            <li>
+              <NavLink
+                className={setClass}
+                to="cast"
+                state={{ from: backLinkHRef }}
+              >
+                Cast
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={setClass}
+                to="reviews"
+                state={{ from: backLinkHRef }}
+              >
+                Reviews
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className={details}>
+          <Outlet />
+        </div>
       </section>
     </>
   );
